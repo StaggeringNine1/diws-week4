8482370ec9f0bf75bb8f446260e1ec3ed1aba6c9
Sam Williams <blackeye793@gmail.com>
2021-10-13 16:53:08 +0100

    Added POPOSList and POPOSSpace objects

---
+++ src/POPOSList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import POPOSSpace from './POPOSSpace';
+
+function POPOSList() {
+    return (
+        <div>
+            <POPOSSpace/>
+            <POPOSSpace/>
+            <POPOSSpace/>
+            <POPOSSpace/>
+        </div>
+    )
+}
+
+export default POPOSList
